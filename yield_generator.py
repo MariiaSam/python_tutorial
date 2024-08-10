8b74c5923047dac4b1c24952cac13ebaa0be32e9
MariiaSam <kontragent@ukr.net>
2024-08-10 16:49:11 +0300

    add

--- yield_generator.py
+++ yield_generator.py
@@ -29,3 +29,16 @@ for number in count_down(5):
 1
 '''
 
+def read_lines(file_path):
+    with open(file_path, 'r', encoding="utf-8") as file:
+        for line in file:
+            yield line.strip()
+
+# Використання генератора для читання рядків з файлу
+for line in read_lines("my_file.txt"):
+    print(line)
+
+
+
+# Генератори можна перебирати в циклі for або вручну за допомогою функції next().
+
